79f280038b9f3390fb5410605a74081aefe24d1b
lilnoes <ishimweleon@gmail.com>
2025-03-25 22:40:51 -0400

    feat: added custom navigation

--- covert/app.py
+++ covert/app.py
@@ -4,23 +4,35 @@ from covert.scripts.combine.page import main as combine_main
 
 # from scripts.default.default import main as default_main
 
-pages = [
-    st.Page(
-        backfill_main,
-        title="Backfill",
-        icon=":material/arrow_back_ios:",
-        url_path="/backfill",
-    ),
-    st.Page(
-        combine_main,
-        title="Combine",
-        icon=":material/arrow_back_ios:",
-        url_path="/combine",
-    ),
-    st.Page(
-        "scripts/default/default.py", title="Default", icon=":material/arrow_back_ios:"
-    ),
-]
+pages = {
+    "Files Processing": [
+        st.Page(
+            backfill_main,
+            title="Backfill",
+            icon=":material/arrow_back_ios:",
+            url_path="/backfill",
+        ),
+        st.Page(
+            combine_main,
+            title="Combine",
+            icon=":material/arrow_back_ios:",
+            url_path="/combine",
+        ),
+        st.Page(
+            "scripts/default/default.py",
+            title="Default",
+            icon=":material/arrow_back_ios:",
+        ),
+    ]
+}
+
+st.sidebar.title("Covert Exports")
+with st.sidebar:
+    for key, values in pages.items():
+        expander = st.sidebar.expander(key)
+        for page in values:
+            expander.page_link(page)
+        st.sidebar.divider()
 
 pg = st.navigation(pages)
 pg.run()
