39aa76a3ef02902202bff5c547ca2dd876927367
lilnoes <ishimweleon@gmail.com>
2025-04-06 19:59:17 -0400

    chore: removed streamlit tests

--- covert/tests/test_smoke_test_pages.py
+++ covert/tests/test_smoke_test_pages.py
@@ -1,12 +1,14 @@
 from streamlit.testing.v1 import AppTest
+import unittest
 from covert.app import pages
 
 
-def test_smoke_test_pages():
-    """
-    Test that all pages are linked in the sidebar, and no exceptions are raised.
-    """
-    app = AppTest.from_file("covert/app.py", default_timeout=100).run()
-    links = app.get("page_link")
-    pages_count = [1 for items in pages.values() for _ in items]
-    assert len(links) == sum(pages_count)
+class TestSmokeTestPages(unittest.TestCase):
+    def test_smoke_test_pages(self):
+        """
+        Test that all pages are linked in the sidebar, and no exceptions are raised.
+        """
+        app = AppTest.from_file("covert/app.py", default_timeout=100).run()
+        links = app.get("page_link")
+        pages_count = [1 for items in pages.values() for _ in items]
+        self.assertEqual(len(links), sum(pages_count))
